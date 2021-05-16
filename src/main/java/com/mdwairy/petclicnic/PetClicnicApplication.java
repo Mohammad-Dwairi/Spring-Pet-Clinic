bbdc3e07856c61309b2c522d0657c7928fc0b789
Mohammad-Dwairi <mdwairy@gmail.com>
2021-05-16 17:03:08 +0300

    Initial commit

---
+++ src/main/java/com/mdwairy/petclicnic/PetClicnicApplication.java
@@ -0,0 +1,13 @@
+package com.mdwairy.petclicnic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClicnicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClicnicApplication.class, args);
+    }
+
+}
