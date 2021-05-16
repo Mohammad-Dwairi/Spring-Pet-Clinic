2072b0791dc68b8a217fda256e644e653762bc27
Mohammad-Dwairi <mdwairy@gmail.com>
2021-05-17 02:47:14 +0300

    List vets in vets home page. Closes #13.

--- pet-clinic-web/src/main/java/com/mdwairy/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/mdwairy/petclinic/controllers/VetController.java
@@ -1,13 +1,24 @@
 package com.mdwairy.petclinic.controllers;
 
+import com.mdwairy.petclinic.services.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    @Autowired
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @GetMapping({"/vets", "/vets/index", "/vets.index"})
-    public String vetsHome() {
+    public String vetsHome(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/home";
     }
 }
