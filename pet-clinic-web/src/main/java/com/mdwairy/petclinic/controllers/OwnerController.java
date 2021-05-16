d191681b7dfd7dfccf412ab3ba64a455f528beb3
Mohammad-Dwairi <mdwairy@gmail.com>
2021-05-17 02:41:16 +0300

    List owners in owners home page. Closes #12.

--- pet-clinic-web/src/main/java/com/mdwairy/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/mdwairy/petclinic/controllers/OwnerController.java
@@ -1,13 +1,24 @@
 package com.mdwairy.petclinic.controllers;
 
+import com.mdwairy.petclinic.services.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    @Autowired
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @GetMapping("/owners")
-    public String ownerHome() {
+    public String ownerHome(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/home";
     }
 }
