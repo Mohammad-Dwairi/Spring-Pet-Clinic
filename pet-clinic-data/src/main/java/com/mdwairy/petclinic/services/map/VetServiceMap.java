5293bd98436b9ead44698a2be5b9d855fdc8e2a6
Mohammad-Dwairi <mdwairy@gmail.com>
2021-05-18 18:15:51 +0300

    Override save method in VetServiceMap to handle null saves. Closes #30.

--- pet-clinic-data/src/main/java/com/mdwairy/petclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/mdwairy/petclinic/services/map/VetServiceMap.java
@@ -1,9 +1,30 @@
 package com.mdwairy.petclinic.services.map;
 
+import com.mdwairy.petclinic.model.Speciality;
 import com.mdwairy.petclinic.model.Vet;
+import com.mdwairy.petclinic.services.SpecialityService;
 import com.mdwairy.petclinic.services.VetService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+
+    private final SpecialityService specialtyService;
+
+    public VetServiceMap(SpecialityService specialtyService) {
+        this.specialtyService = specialtyService;
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        if (!object.getSpecialities().isEmpty()){
+            object.getSpecialities().forEach(speciality -> {
+                if(speciality.getId() == null){
+                    Speciality savedSpecialty = specialtyService.save(speciality);
+                    speciality.setId(savedSpecialty.getId());
+                }
+            });
+        }
+        return super.save(object);
+    }
 }
