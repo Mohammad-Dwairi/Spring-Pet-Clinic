fd0348ea62cd427de8d0b006f5fbc67ff3d8b4dd
Mohammad-Dwairi <mdwairy@gmail.com>
2021-05-18 18:18:08 +0300

    Override save method in VisitServiceMap to handle null saves. Closes #31.

--- pet-clinic-data/src/main/java/com/mdwairy/petclinic/services/map/VisitServiceMap.java
+++ pet-clinic-data/src/main/java/com/mdwairy/petclinic/services/map/VisitServiceMap.java
@@ -6,4 +6,13 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Visit save(Visit visit) {
+        if(visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null
+                || visit.getPet().getOwner().getId() == null){
+            throw new RuntimeException("Invalid Visit");
+        }
+        return super.save(visit);
+    }
 }
