b00ce7f5abfb245385668703df69d8c2fe23631f
Mohammad-Dwairi <mdwairy@gmail.com>
2021-05-24 18:44:39 +0300

    Convert POJOs to JPA entity classes. Closes #33

--- pet-clinic-data/src/main/java/com/mdwairy/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/mdwairy/petclinic/model/BaseEntity.java
@@ -3,10 +3,17 @@ package com.mdwairy.petclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Getter
 @Setter
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name= "id")
     private Long id;
+
 }
--- pet-clinic-data/src/main/java/com/mdwairy/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/mdwairy/petclinic/model/Owner.java
@@ -3,16 +3,26 @@ package com.mdwairy.petclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
 
+    @Column(name = "address")
     private String address;
+
+    @Column(name = "city")
     private String city;
+
+    @Column(name = "phone_number")
     private String phoneNumber;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
 }
