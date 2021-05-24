d39ac5316bdf53869605237727bd1367cafda478
Mohammad-Dwairi <mdwairy@gmail.com>
2021-05-24 20:04:43 +0300

    Create Vet Spring Data JPA Service. Closes #36

---
+++ pet-clinic-data/src/main/java/com/mdwairy/petclinic/services/springdata/VetServiceJpa.java
@@ -0,0 +1,48 @@
+package com.mdwairy.petclinic.services.springdata;
+
+import com.mdwairy.petclinic.model.Vet;
+import com.mdwairy.petclinic.repositories.VetRepository;
+import com.mdwairy.petclinic.services.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class VetServiceJpa implements VetService {
+
+    private final VetRepository vetRepository;
+
+    @Autowired
+    public VetServiceJpa(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return vetRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet type) {
+        return vetRepository.save(type);
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public void delete(Vet type) {
+        vetRepository.delete(type);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        vetRepository.deleteById(id);
+    }
+}
