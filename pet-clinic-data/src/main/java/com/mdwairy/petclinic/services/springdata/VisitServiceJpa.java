abc73fe1084e4807a5592c097503571418ef0463
Mohammad-Dwairi <mdwairy@gmail.com>
2021-05-24 20:37:37 +0300

    Create Visit Spring Data JPA Service. Closes #39

---
+++ pet-clinic-data/src/main/java/com/mdwairy/petclinic/services/springdata/VisitServiceJpa.java
@@ -0,0 +1,48 @@
+package com.mdwairy.petclinic.services.springdata;
+
+import com.mdwairy.petclinic.model.Visit;
+import com.mdwairy.petclinic.repositories.VisitRepository;
+import com.mdwairy.petclinic.services.VisitService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class VisitServiceJpa implements VisitService {
+
+    private final VisitRepository visitRepository;
+
+    @Autowired
+    public VisitServiceJpa(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return visitRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit type) {
+        return visitRepository.save(type);
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+        visitRepository.findAll().forEach(visits::add);
+        return visits;
+    }
+
+    @Override
+    public void delete(Visit type) {
+        visitRepository.delete(type);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        visitRepository.deleteById(aLong);
+    }
+}
