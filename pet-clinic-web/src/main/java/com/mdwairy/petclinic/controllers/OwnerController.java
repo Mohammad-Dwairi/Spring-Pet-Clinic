a810794aaf93c02f60053b76f9ce9e32db5cb46e
Mohammad-Dwairi <mdwairy@gmail.com>
2021-05-25 19:54:24 +0300

    Add ownerDetails.html and controller method. Closes #46

--- pet-clinic-web/src/main/java/com/mdwairy/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/mdwairy/petclinic/controllers/OwnerController.java
@@ -5,9 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/owners")
 public class OwnerController {
 
     private final OwnerService ownerService;
@@ -17,9 +19,16 @@ public class OwnerController {
         this.ownerService = ownerService;
     }
 
-    @GetMapping("/owners")
+    @GetMapping("/")
     public String ownerHome(Model model) {
         model.addAttribute("owners", ownerService.findAll());
         return "owners/home";
     }
+
+    @GetMapping("/{ownerId}")
+    public String ownerDetails(Model model, @PathVariable Long ownerId) {
+        model.addAttribute("owner", ownerService.findById(ownerId));
+        return "owners/ownerDetails";
+    }
+
 }
